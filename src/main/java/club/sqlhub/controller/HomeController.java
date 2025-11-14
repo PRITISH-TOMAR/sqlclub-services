23feac981e5c4234d0d6b1ed756bd05fd84b0447
Pritish Tomar <pritishtomar2004@gmail.com>
2025-11-14 15:45:32 +0530

    add-project-structure-setup

---
+++ src/main/java/club/sqlhub/controller/HomeController.java
@@ -0,0 +1,11 @@
+package club.sqlhub.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
+
+
+public class HomeController {
+    
+    @GetExchange
+}
