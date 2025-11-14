ae6c5d7a5ebc2eb7c6f9e7e11a088321d813ea46
Pritish Tomar <pritishtomar2004@gmail.com>
2025-11-14 17:52:18 +0530

    move-main-file-to-root-location

---
+++ src/main/java/club/sqlhub/SqlHubBackendApplication.java
@@ -0,0 +1,13 @@
+package club.sqlhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlHubBackendApplication.class, args);
+	}
+
+}
